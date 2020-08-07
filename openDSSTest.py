0ea49d30829605b17981e53b3d12558b82d0e04c
christian <chrisrocks40@gmail.com>
2020-08-07 15:57:43 -0700

    openDSS environment test

--- openDSSTest.py
+++ openDSSTest.py
@@ -2,5 +2,21 @@ import gym
 
 env = gym.make('gym_openDSS:openDSS-v0')
 
-env.step()
-env.reset()
+# Box(41,) means that it is a Vector with 41 components
+print("Observation space:", env.observation_space)
+# Discrete(4) means that there is two discrete actions
+print("Action space:", env.action_space)
+
+# Get initial set of observations (states)
+obs = env.reset()
+
+# Test actions
+nsteps = 10
+for step in range(nsteps):
+    print("\nStep: ", step)
+    action = env.action_space.sample()
+    print("Sampled action:", action)
+    obs, reward, done, info = env.step(action)
+    # print('obs=', obs)
+    print('reward=', reward)
+
